bb2a8872a5697313635981ba8d305615587acbac
Jon Church <me@jonchurch.com>
2019-12-17 11:57:02 -0500

    code changes per review, and prettier formatting

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-const fs = require("fs");
-const csv = require("csv-parser");
-const randomWords = require("random-words");
+const fs = require('fs');
+const csv = require('csv-parser');
+const randomWords = require('random-words');
 
 const users = [];
 
 function generateUsername(firstname, surname) {
-  return (firstname[0] + "-" + surname).toLowerCase();
+  return `${firstname[0]}-${surname}`.toLowerCase();
 }
 
-fs.createReadStream("input.csv")
+fs.createReadStream('input.csv')
   .pipe(csv())
-  .on("data", function(row) {
+  .on('data', function(row) {
     const username = generateUsername(row.Firstname, row.Surname);
-    const password = randomWords(3).join("-");
+    const password = randomWords(3).join('-');
 
     const user = {
       username,
       firstname: row.Firstname,
       surname: row.Surname,
       roles: row.Roles,
-      password
+      password,
     };
 
     users.push(user);
   })
-  .on("end", function() {
+  .on('end', function() {
     console.table(users);
     writeToCSVFile(users);
   });
 
 function writeToCSVFile(users) {
-  const filename = "output.csv";
+  const filename = 'output.csv';
 
-  fs.writeFileSync(filename, extractAsCSV(users));
-  console.log(`saved as ${filename}`);
+  fs.writeFile(filename, extractAsCSV(users), err => {
+    if (err) {
+      console.log('Error writing to csv file', err);
+    } else {
+      console.log(`saved as ${filename}`);
+    }
+  });
 }
 
 function extractAsCSV(users) {
-  const header = ["Username, Password, Roles"];
-  const rows = users.map(user => [
-    `${user.username}, ${user.password}, ${user.roles}`
-  ]);
+  const header = ['Username, Password, Roles'];
+  const rows = users.map(
+    user => `${user.username}, ${user.password}, ${user.roles}`
+  );
 
-  return [...header, ...rows].join("\n");
+  return header.concat(rows).join('\n');
 }
